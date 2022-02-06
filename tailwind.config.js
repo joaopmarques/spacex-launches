6a87262a9fdaa65812777630cca4a3f657fa89f6
João P. Marques <joaop.marques@outlook.com>
2022-02-06 14:24:16 +0000

    Tailwind CSS configuration + font settings

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      display: ['Inter', 'system-ui', 'sans-serif'],
+      body: ['Inter', 'system-ui', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
